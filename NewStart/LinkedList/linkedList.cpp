78ca5c2ee1206cb260dd445d063f554eba2f06d7
Deepak <0822deepakverma@gmail.com>
2022-05-01 11:50:07 +0530

    CRUD operations in linkedlist

--- NewStart/LinkedList/linkedList.cpp
+++ NewStart/LinkedList/linkedList.cpp
@@ -14,16 +14,37 @@ void printList(Node *n){
         n=n->next;
     }
 }
+
+
+int countList(Node *n){
+    int count=0;
+    while(n!=NULL){
+  count++;
+  n=n->next;
+    }
+    return count;
+}
+
+int sumList(Node *n){
+    int sum=0;
+    while(n!=NULL){
+        sum+=n->data;
+        n=n->next;
+    }
+    return sum;
+}
+
+
 int main(){
-    Node *head=NULL;
-    Node *second=NULL;
-    Node *third =NULL;
+    Node *head=new Node();
+    Node *second=new Node();
+    Node *third =new Node();
 
-    head= new Node();
-    second=new Node();
-    third=new Node();
+    // head= new Node();
+    // second=new Node();
+    // third=new Node();
 
-    head->data=1;
+    head->data=2;
     head->next=second;
 
     second->data=2;
@@ -31,8 +52,30 @@ int main(){
 
     third->data=3;
     third->next=NULL;
+    // inserting at first position
+    Node *newNode=new Node();
+    newNode->data=5;
+    newNode->next=head;
+
+
+    // inserting at last position
+    Node *newNode2=new Node();
+    newNode2->data=9;
+    newNode2->next=NULL;
+    third->next=newNode2;
+
 
     printList(head);
+    cout<<endl;
+    printList(newNode);
+    cout<<endl;
+    cout<<"No. of nodes: "<< countList(head)<<endl;
+
+    cout<<"No. of nodes: "<< countList(newNode2)<<endl;
+    cout<<"Sum of nodes: "<<sumList(head);
+
+
+
     return 0;
 
 }
