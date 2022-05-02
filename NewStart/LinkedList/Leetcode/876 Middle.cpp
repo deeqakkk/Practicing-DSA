39e7a68162633feef3caff34d4b9ad1abcb21dde
Deepak <0822deepakverma@gmail.com>
2022-05-02 13:36:20 +0530

    Update 876 Middle.cpp

--- NewStart/LinkedList/Leetcode/876 Middle.cpp
+++ NewStart/LinkedList/Leetcode/876 Middle.cpp
@@ -2,20 +2,22 @@
 // https://leetcode.com/problems/middle-of-the-linked-list/
 // Difficulty: Easy
 // Tags: Linked List
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    
+};
+
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast && fast->next) {
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
+        {
             slow = slow->next;
             fast = fast->next->next;
         }
