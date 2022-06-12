5811e1492db4015b9f67a113b097dca542393ccd
Deepak <0822deepakverma@gmail.com>
2022-06-12 11:43:43 +0530

    Create 169-MajorityElement.cpp

---
+++ Leetcode/169-MajorityElement.cpp
@@ -0,0 +1,22 @@
+// best apporach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+     int count=0;
+        int element=nums[0];
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0)
+                element=nums[i];
+            
+            if(element==nums[i])
+                count++;
+            else count--;
+        }
+        return element;
+        
+    }
+};
+
+// optimal approach
