58fa1cb391a11a3f31c7ee7a04ef17a9ff433dfb
Deepak <0822deepakverma@gmail.com>
2022-06-30 10:24:14 +0530

    Update 160-IntersectionOfTwoLInkedList.cpp

--- Leetcode/160-IntersectionOfTwoLInkedList.cpp
+++ Leetcode/160-IntersectionOfTwoLInkedList.cpp
@@ -48,3 +48,50 @@ public:
 };
 
 // Optimal Approach 2
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    
+        ListNode *d1=headA;
+        ListNode *d2=headB;
+        
+        int c1=0,c2=0;
+        
+        while(d1)
+        {
+             c1++;
+            d1=d1->next;
+        }
+        d1=headA;
+         while(d2)
+        {
+             c2++;
+            d2=d2->next;
+        }
+        d2=headB;
+        
+        int diff=abs(c1-c2);
+        
+        if(c1>c2)
+            while(diff--)
+                d1=d1->next;
+        else 
+            while(diff--)
+                d2=d2->next;
+        
+        while(d1 || d2)
+        {
+            if(d1==d2)
+                return d2;
+            else {
+           d1=d1->next;
+                d2=d2->next;
+            }
+        }
+        
+        return NULL;
+                
+    
+    }
+};
