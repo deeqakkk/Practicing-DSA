9e3bccd87ccd6aa62dc6613dff446677f7c027db
Deepak <0822deepakverma@gmail.com>
2022-06-30 09:42:50 +0530

    Create 160-IntersectionOfTwoLInkedList.cpp

---
+++ Leetcode/160-IntersectionOfTwoLInkedList.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *temp1=headA;
+        ListNode *temp2=headB;
+        
+        while(temp1)
+        {
+            while(temp2)
+            {
+                if(temp2==temp1)
+                    return temp1;
+                else 
+                    temp2=temp2->next;
+            }
+            temp1=temp1->next;
+            temp2=headB;
+        }
+        return NULL;
+    }
+};
+
+// Optimal Approach
